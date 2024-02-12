c0ffc9358d17f06aa9136127d15bae40e8ec4939
Fletcher Stone <Fletcher.w.stone@gmail.com>
2024-02-12 00:08:49 -0500

    Completes Lab

--- index.js
+++ index.js
@@ -1,1 +1,20 @@
-// Write your code here!
+// Get a reference to the main element
+const mainElement = document.getElementById('main');
+
+// Check if the main element exists
+if (mainElement) {
+  // Get a reference to its parent node
+  const parentElement = mainElement.parentNode;
+  
+  // Remove the main element from its parent node
+  parentElement.removeChild(mainElement);
+}
+const newHeader= document.createElement('h1');
+newHeader.textContent = 'This is a new header';
+document.body.appendChild(newHeader);
+newHeader.id='victory';
+document.body.appendChild(newHeader)
+
+const yourName= "Fletcher"
+newHeader.textContent= yourName + "is the champion"
+document.body.appandChild(newHeader)
